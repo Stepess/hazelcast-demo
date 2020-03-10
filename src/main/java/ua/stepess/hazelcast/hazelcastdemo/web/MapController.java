ae17442f990d498354c9abc746c88c0b41d2094a
Stepess <stepanersh@gmail.com>
2020-03-11 00:02:46 +0200

    add controller

--- src/main/java/ua/stepess/hazelcast/hazelcastdemo/web/MapController.java
+++ src/main/java/ua/stepess/hazelcast/hazelcastdemo/web/MapController.java
@@ -1,12 +1,12 @@
 package ua.stepess.hazelcast.hazelcastdemo.web;
 
 import com.hazelcast.core.HazelcastInstance;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/v1/hazelcast")
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/map")
 public class MapController {
 
     private final HazelcastInstance hazelcastInstance;
@@ -16,10 +16,20 @@ public class MapController {
     }
 
     @PostMapping("/{key}")
-    public String write(@PathVariable String key,
-                        @RequestBody String content) {
+    public String write(@PathVariable String key, @RequestBody String content) {
         var map = hazelcastInstance.getMap("data");
         map.set(key, content);
         return "putted";
+    }
+
+    @GetMapping("/{key}")
+    public String read(@PathVariable String key) {
+        var map = hazelcastInstance.<String, String>getMap("data");
+        return map.get(key);
+    }
+
+    @GetMapping
+    public Map<String, String> readAll() {
+        return hazelcastInstance.<String, String>getMap("data");
     }
 }
