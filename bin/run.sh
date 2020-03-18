bea5fe31805bffd6f326f7896ad103e4a4f673cd
Stepess <stepanersh@gmail.com>
2020-03-18 23:25:30 +0200

    rewrite script

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,10 @@
-./gradlew clean bootRun --args='--hazelcast.port=5701'
+# build
+./gradlew clean bootJar
+
+cd build/libs || exit
+
+# run first instance
+java -jar hazelcast-demo-0.0.1-SNAPSHOT.jar --server.port=8080 --hazelcast.port=5701 &
+
+# run second instance
+java -jar hazelcast-demo-0.0.1-SNAPSHOT.jar --server.port=8081 --hazelcast.port=5702
