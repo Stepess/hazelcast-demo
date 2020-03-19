05289a15e79de2ad3a1cb8695924cd5202c340c8
Stepess <stepanersh@gmail.com>
2020-03-19 09:47:36 +0200

    fix script

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# cleanup backgroung processes
+trap "kill 0" EXIT
+
 # build
 ./gradlew clean bootJar
 
@@ -7,4 +12,7 @@ cd build/libs || exit
 java -jar hazelcast-demo-0.0.1-SNAPSHOT.jar --server.port=8080 --hazelcast.port=5701 &
 
 # run second instance
-java -jar hazelcast-demo-0.0.1-SNAPSHOT.jar --server.port=8081 --hazelcast.port=5702
+java -jar hazelcast-demo-0.0.1-SNAPSHOT.jar --server.port=8081 --hazelcast.port=5702 &
+
+wait
+
