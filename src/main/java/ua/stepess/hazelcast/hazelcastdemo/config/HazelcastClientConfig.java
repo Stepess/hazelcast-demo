66cac6fb84041ac7b4cef3daf6c6d9f58594850e
Stepan Yershov <stepan.yershov@ticketmaster.com>
2020-03-20 11:16:30 +0200

    fixed run

--- src/main/java/ua/stepess/hazelcast/hazelcastdemo/config/HazelcastClientConfig.java
+++ src/main/java/ua/stepess/hazelcast/hazelcastdemo/config/HazelcastClientConfig.java
@@ -1,20 +1,19 @@
 package ua.stepess.hazelcast.hazelcastdemo.config;
 
-import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.core.HazelcastInstance;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ua.stepess.hazelcast.hazelcastdemo.config.properties.HazelcastProperties;
 
-import java.util.List;
-
 @Configuration
 @RequiredArgsConstructor
 @EnableConfigurationProperties(HazelcastProperties.class)
 public class HazelcastClientConfig {
+
+    private static final String HOST = "192.168.1.10";
+    private static final String COLON = ":";
 
     private final HazelcastProperties properties;
 
@@ -22,17 +21,11 @@ public class HazelcastClientConfig {
     public ClientConfig clientConfig() {
         var config = new ClientConfig();
 
-        var address = "http://172.19.0.3:" + properties.getPort();
+        var address = HOST + COLON + properties.getPort();
         config.getNetworkConfig()
                .addAddress(address);
-
         return config;
     }
 
-    @Bean
-
-    public HazelcastInstance instance(ClientConfig clientConfig) {
-        return HazelcastClient.newHazelcastClient(clientConfig);
-    }
 
 }
